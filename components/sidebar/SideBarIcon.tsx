00c5c505252ad5a137686620736f8d49e90bb3ef
Andrew Berty <andrewgeorgeberty@gmail.com>
2023-12-28 18:49:57 +0200

    integrate framer motion & sidebar toggle animations

--- components/sidebar/SideBarIcon.tsx
+++ components/sidebar/SideBarIcon.tsx
@@ -6,26 +6,28 @@ interface Props {
 	children: React.ReactNode
 	link?: string
 	tooltipText: string
-	func?: Function
+	onClick?: Function
+	className?: string
 }
 
-function SideBarIcon({ link, children, tooltipText, func }: Props) {
+function SideBarIcon({ link, children, tooltipText, onClick, className }: Props) {
 	return (
-		<TooltipProvider>
-			<Tooltip>
-				<TooltipTrigger className='opacity-50 hover:opacity-100 transition-opacity'>
-					{link ? (
-						<Link href={link}>{children}</Link>
-					) : (
-						//bug
-						<div onClick={func ? () => func() : () => {}}>{children}</div>
-					)}
-				</TooltipTrigger>
-				<TooltipContent side='left' sideOffset={20}>
-					<p>{tooltipText}</p>
-				</TooltipContent>
-			</Tooltip>
-		</TooltipProvider>
+		<div className={className}>
+			<TooltipProvider>
+				<Tooltip>
+					<TooltipTrigger className='opacity-50 hover:opacity-100 transition-opacity'>
+						{link ? (
+							<Link href={link}>{children}</Link>
+						) : (
+							<div onClick={onClick ? () => onClick() : () => {}}>{children}</div>
+						)}
+					</TooltipTrigger>
+					<TooltipContent side='left' sideOffset={20}>
+						<p>{tooltipText}</p>
+					</TooltipContent>
+				</Tooltip>
+			</TooltipProvider>
+		</div>
 	)
 }
 
--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { Separator } from '@/components/ui/separator'
+import { useAnimate } from 'framer-motion'
 import {
 	Aperture,
 	CircleUser,
@@ -12,58 +13,82 @@ import {
 	Search,
 	ShoppingCart
 } from 'lucide-react'
-
+import { useEffect, useState } from 'react'
 import SideBarIcon from './SideBarIcon'
 
 function Sidebar() {
-	function handleHideSidebar() {}
+	const [scope, animate] = useAnimate()
+	const [isVisible, setIsVisible] = useState(false)
+
+	useEffect(() => {
+		if (isVisible) {
+			animate('aside', { x: [0, -100] })
+			animate('aside', { opacity: 0 })
+			animate('.show-sidebar', { x: [-100, 12] })
+		} else {
+			animate('aside', { x: [-100, 0] })
+			animate('aside', { opacity: 1 })
+			animate('.show-sidebar', { x: [12, -100] })
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [isVisible])
+
 	function handleCommandPalette() {}
 	function handleSearchPrompt() {}
 
 	return (
-		<aside className='absolute w-[var(--sidebar-width)] flex flex-col items-center py-4 justify-between h-screen border-r border-r-muted/30 bg-background'>
-			<ul className='flex flex-col gap-4'>
-				<SideBarIcon link='/' tooltipText='Aperture'>
-					<Aperture />
-				</SideBarIcon>
+		<div ref={scope}>
+			<aside className='absolute w-[var(--sidebar-width)] flex flex-col items-center py-4 justify-between h-screen border-r border-r-muted/30 bg-background'>
+				<ul className='flex flex-col gap-4'>
+					<SideBarIcon link='/' tooltipText='Aperture'>
+						<Aperture />
+					</SideBarIcon>
 
-				<Separator className='bg-muted/50' />
+					<Separator className='bg-muted/50' />
 
-				<SideBarIcon tooltipText='Command Palette' func={handleCommandPalette}>
-					<Command />
-				</SideBarIcon>
-				<SideBarIcon tooltipText='Command Palette' func={handleSearchPrompt}>
-					<Search />
-				</SideBarIcon>
-				<SideBarIcon link='/browse' tooltipText='Browse'>
-					<GalleryHorizontalEnd />
-				</SideBarIcon>
-				<SideBarIcon link='/cart' tooltipText='My Cart'>
-					<ShoppingCart />
-				</SideBarIcon>
-			</ul>
+					<SideBarIcon tooltipText='Command Palette' onClick={handleCommandPalette}>
+						<Command />
+					</SideBarIcon>
+					<SideBarIcon tooltipText='Command Palette' onClick={handleSearchPrompt}>
+						<Search />
+					</SideBarIcon>
+					<SideBarIcon link='/browse' tooltipText='Browse'>
+						<GalleryHorizontalEnd />
+					</SideBarIcon>
+					<SideBarIcon link='/cart' tooltipText='My Cart'>
+						<ShoppingCart />
+					</SideBarIcon>
+				</ul>
 
-			<ul className='flex flex-col gap-4'>
-				<SideBarIcon link='https://github.com/andrew-george' tooltipText='Github'>
-					<Github />
-				</SideBarIcon>
-				<SideBarIcon link='https://andrewberty.vercel.app' tooltipText='Portfolio'>
-					<Globe />
-				</SideBarIcon>
-				<SideBarIcon link='https://www.linkedin.com/in/andrew-berty' tooltipText='Portfolio'>
-					<Linkedin />
-				</SideBarIcon>
+				<ul className='flex flex-col gap-4'>
+					<SideBarIcon link='https://github.com/andrew-george' tooltipText='Github'>
+						<Github />
+					</SideBarIcon>
+					<SideBarIcon link='https://andrewberty.vercel.app' tooltipText='Portfolio'>
+						<Globe />
+					</SideBarIcon>
+					<SideBarIcon link='https://www.linkedin.com/in/andrew-berty' tooltipText='Portfolio'>
+						<Linkedin />
+					</SideBarIcon>
 
-				<Separator className='bg-muted/50' />
+					<Separator className='bg-muted/50' />
 
-				<SideBarIcon tooltipText='Guest'>
-					<CircleUser />
-				</SideBarIcon>
-				<SideBarIcon tooltipText='Hide Sidebar' func={() => handleHideSidebar()}>
-					<PanelLeftClose />
-				</SideBarIcon>
-			</ul>
-		</aside>
+					<SideBarIcon tooltipText='Guest'>
+						<CircleUser />
+					</SideBarIcon>
+
+					<SideBarIcon tooltipText='Hide Sidebar' onClick={() => setIsVisible(prev => !prev)}>
+						<PanelLeftClose />
+					</SideBarIcon>
+				</ul>
+			</aside>
+			<SideBarIcon
+				className='show-sidebar absolute bottom-4 -translate-x-full'
+				tooltipText='Show Sidebar'
+				onClick={() => setIsVisible(prev => !prev)}>
+				<PanelLeftClose />
+			</SideBarIcon>
+		</div>
 	)
 }
 
